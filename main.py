11aebbbb6483701aef52574f0f1b08eb530c04f4
Michael Phillips <michaelphillips@vt.edu>
2025-02-20 14:30:34 -0500

    removed bollinger and added in macd. better indicator.

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def ParseData(results):
 
     return [dates, closing_prices, open_prices, high_prices, low_prices]
 
-def CalculateRSI(closing_prices, period = 14):
+def calculateRSI(closing_prices, period = 14):
     totalRSI = []
     price_changes = [closing_prices[i] - closing_prices[i - 1] for i in range(1, len(closing_prices))]
     gains = [max(change, 0) for change in price_changes]
@@ -34,65 +34,69 @@ def CalculateRSI(closing_prices, period = 14):
         totalRSI.append(100 - (100 / (1 + rs)))
     return totalRSI
 
-def calculateBollingerBands(prices, period=14, num_std_dev=2):
-    rolling_mean = []
-    rolling_std = []
-    for i in range(len(prices) - period + 1):
-        window_prices = prices[i:i + period]
-        mean = sum(window_prices) / period
-        std_dev = (sum((p - mean) ** 2 for p in window_prices) / (period - 1)) ** 0.5  # Sample std dev
-        rolling_mean.append(mean)
-        rolling_std.append(std_dev)
-    upper_band = [m + (num_std_dev * s) for m, s in zip(rolling_mean, rolling_std)]
-    lower_band = [m - (num_std_dev * s) for m, s in zip(rolling_mean, rolling_std)]
-    return [rolling_mean, upper_band, lower_band]
+def computeEMA(data, period):
+    ema = []
+    alpha = 2 / (period + 1)
+    for i, value in enumerate(data):
+        if i == 0:
+            ema.append(value)
+        else:
+            ema.append(alpha * value + (1 - alpha) * ema[i - 1])
+    return ema
+
+def calculateMACD(close_prices, fast_period = 12, slow_period = 26, signal_period = 9):
+    ema_fast = computeEMA(close_prices, fast_period)
+    ema_slow = computeEMA(close_prices, slow_period)
+    macd_line = [fast - slow for fast, slow in zip(ema_fast, ema_slow)]
+    signal_line = computeEMA(macd_line, signal_period)
+    return [signal_line, macd_line]
 
 tickers = ["MSFT", "HL", "AMZN", "AAPL", "NFLX", "NVDA", "PLTR", "GOOGL", "META", "TSLA", "JNJ", "JPM", "V", "DIS", "PFE", "CSCO", "XOM", "T", "WMT"]
 CurrentDate = datetime.date.today()
 PreviousDate = CurrentDate - datetime.timedelta(days=7 * 50)
 for ind, ticker in enumerate(tickers):
     try:
-        request = requests.get(f"https://api.polygon.io/v2/aggs/ticker/{ticker}/range/1/week/{PreviousDate.isoformat()}/{CurrentDate.isoformat()}?adjusted=true&sort=asc&limit=50000&apiKey=9cZNiOhwCdE5QpMY8aSsIWh3Z6BVavVC").json()['results']
+        request = requests.get(f"https://api.polygon.io/v2/aggs/ticker/{ticker}/range/1/day/{PreviousDate.isoformat()}/{CurrentDate.isoformat()}?adjusted=true&sort=asc&limit=50000&apiKey=9cZNiOhwCdE5QpMY8aSsIWh3Z6BVavVC").json()['results']
         data = ParseData(request)
         currentPriceData = data[1]
-        print(currentPriceData[-1])
     except:
         time.sleep(15)
         continue
 
-    # Set parameters for indicators
     periodRSI = 14
-    periodBollinger = 14
-    stdDevBollinger = 2
+    periodFastMacd = 12
+    periodSlowMacd = 26
+    signalPeriodMacd = 9
 
-    # Calculate indicators
-    rsi = CalculateRSI(currentPriceData, periodRSI)
-    bollinger = calculateBollingerBands(currentPriceData, periodBollinger, stdDevBollinger)
+    rsi = calculateRSI(currentPriceData, periodRSI)
+    macd = calculateMACD(currentPriceData, periodFastMacd, periodSlowMacd, signalPeriodMacd)
 
-    # Figure for plots
-    fig, axs = plt.subplots(2, 1, figsize=(10, 8))
-    # Plot stock prices
-    axs[0].plot([i for i in range(len(currentPriceData))], currentPriceData)
-    # Plot bollingers over the prices
-    axs[0].plot([i + periodBollinger - 1 for i in range(len(bollinger[0]))], bollinger[0], color='green')
-    axs[0].plot([i + periodBollinger - 1 for i in range(len(bollinger[1]))], bollinger[1], color='red')
-    axs[0].plot([i + periodBollinger - 1 for i in range(len(bollinger[2]))], bollinger[2], color='red')
-    # Set the scale labels of the graph
+    fig, axs = plt.subplots(3,1, figsize=(13,13))
+
+    # Plot the price
+    axs[0].plot([i for i in range(len(currentPriceData))], currentPriceData, label=f"Price: {currentPriceData[-1]}")
     axs[0].set_xlabel("Time Scale (Days)")
-    axs[0].set_ylabel("Price X Bollinger")
+    axs[0].set_ylabel("Price")
+    axs[0].legend(loc="upper left")
 
     # Plot the RSI
     axs[1].plot([i + periodRSI + 1 for i in range(len(rsi))], rsi, label=f"RSI: {rsi[-1]}")
-    # Set bounds for overbought and oversold
-    axs[1].axhline(70, color='red', linestyle='--', label='Threshold 70')
-    axs[1].axhline(30, color='green', linestyle='--', label='Threshold 30')
-    # Share in areas for overbought and oversold
+    axs[1].axhline(70, color='red', linestyle='--')
+    axs[1].axhline(30, color='green', linestyle='--')
     axs[1].fill_between([i + periodRSI + 1 for i in range(len(rsi))], 70, 100, color='red', alpha=0.1)
     axs[1].fill_between([i + periodRSI + 1 for i in range(len(rsi))], 0, 30, color='green', alpha=0.1)
-    # Set the scale labels of the graph
     axs[1].set_xlabel("Time Scale (Days)")
     axs[1].set_ylabel("RSI")
+    axs[1].legend(loc="upper left")
+
+    # Plot the MACD
+    axs[2].plot([i for i in range(len(macd[0]))], macd[0], color="red", label=f"Signal: {macd[0][-1]}")
+    axs[2].plot([i for i in range(len(macd[1]))], macd[1], color="green", label=f"MACD: {macd[1][-1]}")
+    axs[2].set_xlabel("Time Scale (Days)")
+    axs[2].set_ylabel("MACD")
+    axs[2].legend(loc="upper left")
 
     # Save the graph and wait for next api interval
     plt.savefig(f"./Charts/{ticker}_{CurrentDate}.png")
+    plt.show()
     time.sleep(15)
